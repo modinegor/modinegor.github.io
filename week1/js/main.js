37830fea243768528c043a46fb480c5b4138a9f4
Egor Modin <egor_modin@epam.com>
2017-11-28 14:19:20 +0300

    Fix code according to review

--- week1/js/main.js
+++ week1/js/main.js
@@ -1,34 +1,29 @@
-let apikey = '58c65ea125b949f39e840783834dae42';
+const apikey = '58c65ea125b949f39e840783834dae42';
 
 (() => {
-    window.onload = init;
+    window.addEventListener('load', init);
 })(window, document, undefined);
 
-let sources, shown,
+let sources, shown_sources,
     init_filter = false;
 
 function init() {
     showSources();
     showMessage("Chose source to see the news");
 
-    document.getElementById('scroll-up').onclick = scrollSources;
-    document.getElementById('scroll-down').onclick = scrollSources;
+    document.getElementById('scroll-up').addEventListener('click', scrollSources);
+    document.getElementById('scroll-down').addEventListener('click', scrollSources);
 }
 
 function showMessage(message) {
-    let message_div = document.createElement('div');
-
-    message_div.innerHTML = message;
-    message_div.id = 'info-message';
-
-    document.getElementById('shown-articles').appendChild(message_div);
+    document.getElementById('shown-articles').innerHTML = `<div id="info-message">${message}</div>`;
 }
 
 function showSources(category, country, language) {
-    let base_url = 'https://newsapi.org/v2/sources',
-        requests = '';
+    const base_url = 'https://newsapi.org/v2/sources';
+    let requests = '';
 
-    shown = [];
+    shown_sources = [];
 
     if (category)
         requests = `${requests}&category=${category}`;
@@ -40,16 +35,16 @@ function showSources(category, country, language) {
     fetch(`${base_url}?${requests.substr(1)}&apiKey=${apikey}`)
         .then(response => response.json())
         .then((data) => {
-            let shown_sources = document.getElementById('shown-sources');
+            const shown = document.getElementById('shown-sources');
 
-            shown_sources.innerHTML = '';
+            shown.innerHTML = '';
 
             sources = data.sources;
-            sources.shuffle();
+            shuffle_array(sources);
 
             for (let i = 0; i < 3 && i < sources.length - 1; i++) {
-                shown_sources.appendChild((new Source(sources[i])).div);
-                shown.push(i);
+                shown.appendChild((new Source(sources[i])).getHtmlElement());
+                shown_sources.push(i);
             }
 
             if (!init_filter)
@@ -75,71 +70,71 @@ function initFilter() {
         languages.add(source.language);
     }
 
-    for (let category of Array.from(categories).sort()) {
+    for (let category of [...categories].sort()) {
         let option = document.createElement('option');
         option.innerHTML = category;
         category_cmbx.appendChild(option);
     }
-    for (let country of Array.from(countries).sort()) {
+    for (let country of [...countries].sort()) {
         let option = document.createElement('option');
         option.innerHTML = country;
         country_cmbx.appendChild(option);
     }
-    for (let language of Array.from(languages).sort()) {
+    for (let language of [...languages].sort()) {
         let option = document.createElement('option');
         option.innerHTML = language;
         language_cmbx.appendChild(option);
     }
 
-    category_cmbx.onchange = changeFilter;
-    country_cmbx.onchange = changeFilter;
-    language_cmbx.onchange = changeFilter;
+    category_cmbx.addEventListener('change', changeFilter);
+    country_cmbx.addEventListener('change', changeFilter);
+    language_cmbx.addEventListener('change', changeFilter);
 
     init_filter = true;
 }
 
 function changeFilter() {
-    let category_cmbx = document.getElementById('select-category'),
+    const category_cmbx = document.getElementById('select-category'),
         language_cmbx = document.getElementById('select-language'),
         country_cmbx = document.getElementById('select-country');
 
     showSources(
         category_cmbx.options[category_cmbx.selectedIndex].text,
         country_cmbx.options[country_cmbx.selectedIndex].text,
-        language_cmbx.options[language_cmbx.selectedIndex].text)
+        language_cmbx.options[language_cmbx.selectedIndex].text);
 }
 
 function scrollSources(event) {
-    document.getElementById('scroll-up').onclick = null;
-    document.getElementById('scroll-down').onclick = null;
+    document.getElementById('scroll-up').removeEventListener('click', scrollSources);
+    document.getElementById('scroll-down').removeEventListener('click', scrollSources);
 
-    let {target:{id:target}} = event,
+    const {target:{id:target}} = event,
         div = document.getElementById('shown-sources'),
         child = div.childNodes;
 
-    if (target == 'scroll-up' && shown[0] != 0) {
-        shown.pop();
+    if (target === 'scroll-up' && shown_sources[0] !== 0) {
+        shown_sources.pop();
         div.removeChild(child[child.length - 1]);
-        div.insertBefore((new Source(sources[shown[0] - 1])).div, child[0]);
-        shown = [shown[0] - 1, ...shown];
-    } else if (target == 'scroll-down' && shown[shown.length - 1] != sources.length - 1) {
-        shown = shown.slice(1);
+        div.insertBefore((new Source(sources[shown_sources[0] - 1])).getHtmlElement(), child[0]);
+        shown_sources = [shown_sources[0] - 1, ...shown_sources];
+    } else if (target === 'scroll-down' && shown_sources[shown_sources.length - 1] !== sources.length - 1) {
+        shown_sources = shown_sources.slice(1);
         div.removeChild(child[0]);
 
-        let new_item = shown[shown.length - 1] + 1;
+        const new_item = shown_sources[shown_sources.length - 1] + 1;
 
-        div.appendChild((new Source(sources[new_item])).div);
-        shown.push(new_item);
+        div.appendChild((new Source(sources[new_item])).getHtmlElement());
+        shown_sources.push(new_item);
     }
 
-    document.getElementById('scroll-up').onclick = scrollSources;
-    document.getElementById('scroll-down').onclick = scrollSources;
+    document.getElementById('scroll-up').addEventListener('click', scrollSources);
+    document.getElementById('scroll-down').addEventListener('click', scrollSources);
 }
 
 function showArticles(event) {
-    let target = event.target;
+    const target = event.target;
 
-    target.onclick = null;
+    target.removeEventListener('click', showArticles);
 
     fetch(`https://newsapi.org/v2/top-headlines?sources=${target.id}&apiKey=${apikey}`)
         .then(responce => responce.json())
@@ -149,7 +144,7 @@ function showArticles(event) {
             articles_div.innerHTML = '';
 
             for (let article of data.articles)
-                articles_div.appendChild((new Article(article)).div);
+                articles_div.appendChild((new Article(article)).getHtmlElement());
 
         })
         .catch((err) => {
@@ -157,7 +152,7 @@ function showArticles(event) {
             showMessage(`Error: ${err}`);
         });
 
-    target.onclick = showArticles;
+    target.addEventListener('click', showArticles);
 }
 
 class Source {
@@ -170,49 +165,27 @@ class Source {
         } = obj);
     }
 
-    get div() {
-        let div = document.createElement('div'),
-            box = document.createElement('div');
-
-        box.appendChild(this.title);
-        box.appendChild(this.button);
-        div.appendChild(this.logo);
-        div.appendChild(box);
+    getHtmlElement() {
+        const div = document.createElement('div');
 
         div.className = 'source-item';
+        div.innerHTML = `${this.logo}<div>${this.title}${this.button}</div>`;
+
+        div.getElementsByClassName("read-source-button")[0].addEventListener('click', showArticles);
 
         return div;
     }
 
     get logo() {
-        let div = document.createElement('img');
-
-        div.setAttribute('src', `https://icons.better-idea.org/icon?url=${this.url}&size=50`);
-        div.setAttribute('title', this.description);
-        div.className = 'source-logo';
-
-        return div;
+        return `<img src="https://icons.better-idea.org/icon?url=${this.url}&size=50" class="source-logo" title="${this.description}"/>`;
     }
 
     get button() {
-        let div = document.createElement('div');
-
-        div.className = 'read-source-button';
-        div.id = this.id;
-        div.appendChild(document.createTextNode('Read Me'));
-
-        div.onclick = showArticles;
-
-        return div;
+        return `<div class="read-source-button" id="${this.id}">Read Me</div>`
     }
 
     get title() {
-        let div = document.createElement('div');
-
-        div.appendChild(document.createTextNode(this.name));
-        div.className = 'source-name';
-
-        return div;
+        return `<div class="source-name">${this.name}</div>`;
     }
 }
 
@@ -221,73 +194,59 @@ class Article {
         ({
             description: this.description,
             title: this.article_title,
+            publishedAt: this.time,
             url: this.url,
+            source: {id: this.source_id, name: this.source_name},
             urlToImage: this.image_url,
         } = obj);
     }
 
-    get title() {
-        let div = document.createElement('div');
+    get publishedAt() {
+        if (!this.time)
+            return '';
 
-        div.appendChild(document.createTextNode(this.article_title));
-        div.className = 'article-title';
-
-        return div;
+        return ` @ ${(new Date(this.time)).toUTCString()}`;
     }
 
-    get link() {
-        let link = document.createElement('a');
+    get head() {
+        return `<div class="article-head">${this.source_name || this.source_id}${this.publishedAt}</div>`
+    }
 
-        link.setAttribute('href', this.url);
-        link.setAttribute('target', '_blank');
-
-        return link;
+    get title() {
+        return `<div class="article-title">${this.article_title}</div>`;
     }
 
     get image() {
         if (!this.image_url)
-            return null;
+            return '';
 
-        let link = this.link,
-            image_div = document.createElement('div'),
-            image = document.createElement('img');
-
-        image.setAttribute('src', this.image_url);
-
-        image_div.className = 'article-image';
-        image_div.appendChild(image);
-
-        link.appendChild(image_div);
-
-        return link;
+        return `<a href="${this.url}" target="_blank"><div class="article-image"><img src="${this.image_url}"/></div></a>`
     }
 
     get info() {
-        let div = this.image_url ? document.createElement('div') : this.link;
+        if (!this.image_url) {
+            const description = this.description || `Read this article on ${this.source_name || this.source_id} website`;
+            return `<a href="${this.url}" target="_blank" class="article-info">${description}</a>`
+        } else if (this.description)
+            return `<div class="article-info">${this.description}</div>`;
 
-        div.appendChild(document.createTextNode(this.description));
-        div.className = 'article-info';
-
-        return div;
+        return ''
     }
 
-    get div() {
-        let div = document.createElement('div');
+    getHtmlElement() {
+        const div = document.createElement('div');
 
         div.className = 'article-item';
-
-        div.appendChild(this.title);
-        this.image_url && div.appendChild(this.image);
-        div.appendChild(this.info);
+        div.innerHTML = `${this.head}${this.title}${this.image}${this.info}`;
 
         return div;
     }
 }
 
-Array.prototype.shuffle = function() {
-    for (let i = 1; i < this.length; i++) {
+function shuffle_array(array) {
+    for (let i = 1; i < array.length; i++) {
         let rand_i = Math.floor(Math.random() * (i + 1));
 
-        [this[i], this[rand_i]] = [this[rand_i], this[i]];
+        [array[i], array[rand_i]] = [array[rand_i], array[i]];
     }
-};
+}
