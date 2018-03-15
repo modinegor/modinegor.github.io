4e8c1e44f14699fafd1934784f0e7e2d04f67a60
eodin <modin.egor@gmail.com>
2018-03-15 15:22:39 +0300

    React part 2: not finished task

--- react/server/index.js
+++ react/server/index.js
@@ -6,12 +6,23 @@ const cookieParser = require('cookie-parser');
 const blogs = require('./routes/blogRoutes');
 const users = require('./routes/userRoutes');
 const userModel = require('./models/user');
+const path = require("path");
 
 const port = 5002;
 const server = express();
 
 
 server.use(cors());
+
+server.use(express.static(path.join(__dirname, 'dist')));
+
+server.set('views', path.join(__dirname, 'views')); // set views root directory, your .html
+server.set('view engine', 'jade');
+// server.engine('.html', require('ejs').renderFile);
+
+server.get(['/', '/blog', '/login', '/signup'], (req, res, next) => {
+    res.render('./../../index.html');
+});
 
 server.use('/api/blog', blogs);
 
